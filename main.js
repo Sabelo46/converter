e4af5fbc33876452a09a7ff9b70b9812fab0702f
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 10:16:58 +0100

    Checkinssdess

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-if('serviceWorker' in navigator)
-{
+
     navigator.serviceWorker.register('sw.js').then(function(reg){
         console.log('ServiceWorker Registered');
       }).catch(function(err){
         console.log('Not registered',err);
       })
- }
+ 
 
 var get= function(url){
     return new Promise(function(resolve,reject){
@@ -26,23 +25,3 @@ var get= function(url){
     
      });
 };
-get('https://free.currencyconverterapi.com/api/v5/currencies')
-    .then(function(data){
-        console.log("Success",data);
-        let currencies = data.results; 
-        for(c in currencies){ 
-          let option=document.createElement('option');
-          option.value = `${currencies[c].id}`;  
-          let check = currencies[c].id;
-          if(typeof check === 'undefined'){
-            check ='';
-          
-          }
-          option.text =  ` ${check} (${currencies[c].currencyName})`;  
-          expect.appendChild(option);
-          have.appendChild(option.cloneNode(true));
-        }
-
-    }).catch(function(err){
-        console.log('sabelo1');
-    })

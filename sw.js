0a7cd6e5154bc7f95343ac0ca3ebd5301c067804
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 07:03:55 +0100

    Checking

---
+++ sw.js
@@ -0,0 +1,10 @@
+var cache = '';
+self.addEventListener('install',function(event){
+    console.log("installed");
+})
+self.addEventListener('activate',function(event){
+    console.log("installed");
+})
+self.addEventListener('fetch',function(event){
+    console.log("installed");
+})

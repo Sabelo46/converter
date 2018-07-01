718978c821a168133b30bde40717b36842271640
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 09:14:36 +0100

    Checkins

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-if('serviceWorker' in navigator)
-{
+
     navigator.serviceWorker.register('sw.js').then(function(reg){
         console.log('[ServiceWorker] Registered');
       }).catch(function(err){
         console.log('Not registered',err);
       })
-} 
+
 
 var get= function(url){
     return new Promise(function(resolve,reject){

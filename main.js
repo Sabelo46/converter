9e7b4b89c6d08919f079b5915ae7433314b8b6ae
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 10:39:25 +0100

    Checkinssdeseiehf8rhf8rfhr

--- main.js
+++ main.js
@@ -1,2 +1,47 @@
 
-    
+     navigator.serviceWorker.register('sw.js').then(function(reg){
+        console.log('ServiceWorker Registered');
+      }).catch(function(err){
+        console.log('Not registered',err);
+      })
+
+      var get = function(err){
+          return new Promise(function(resolve,reject){
+          var xhr = new XMLHttpRequest();
+          xhr.onreadystatechange = function(){
+              if(xhr.readyState === XMLHttpRequest.DONE)
+              {
+                  if(xhr.status === 200)
+                  {
+                      var result = xhr.responseText;
+                      result = JSON.parse(result);
+                      resolve(result);
+                  }else{
+                      reject(xhr);
+                  }
+              }
+          };
+          xhr.open("GET",url,true);
+          xhr.send();
+        })
+      };
+      get().then(function(response){
+          console.log('Success',response);
+          let currencies = data.results;
+      
+        
+          for(c in currencies){ 
+            let option=document.createElement('option');
+            option.value = `${currencies[c].id}`;  
+            let check = currencies[c].id;
+            if(typeof check === 'undefined'){
+              check ='';
+            
+            }
+            option.text =  ` ${check} (${currencies[c].currencyName})`;  
+            expect.appendChild(option);
+            have.appendChild(option.cloneNode(true));
+          }
+      }).catch(function(err){
+          console.log('Err');
+      })

d88fe9a023ee39824984b929a668a82743b0372c
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 09:24:46 +0100

    Checkinssss

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-
+if('serviceWorker' in navigator)
+{
     navigator.serviceWorker.register('sw.js').then(function(reg){
-        console.log('serviceWorker for sabelo Registered');
+        console.log('[ServiceWorker] Registered');
       }).catch(function(err){
         console.log('Not registered',err);
       })
+ }
+
+var get= function(url){
+    return new Promise(function(resolve,reject){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+          if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                var result = xhr.responseText;
+                result = JSON.parse(result);
+                resolve(result);
+            }else{
+                reject(xhr);
+            }
+          }
+        };
+        xhr.open("GET",url,true);
+        xhr.send();
+    
+     })
+};
+get('https://free.currencyconverterapi.com/api/v5/currencies')
+    .then(function(response){
+        console.log("Success",response);
+    }).catch(function(err){
+        console.log('ERRRR');
+    })

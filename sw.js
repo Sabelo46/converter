bc13e16ae0fcc17c57167e4a164f5516ad51a47e
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 12:20:34 +0100

    ressdefderdedefrhfirifr8gfrgdjfr040frde11frfrdde

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ let cacheName = 'v2';
 var cacheFiles = [
                  'index.html',
                   'main.js',
-                  'sw.js',
                   'node_modules/bootstrap/dist/css/bootstrap.css',
                   'node_modules/bootstrap/dist/js/bootstrap.js',
                   'node_modules/jquery/dist/jquery.js',

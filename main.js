f3fab359913d0c71f150b206b45dcedb3bf98559
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 11:07:28 +0100

    Checsw8de8kinss0000000000e9fjehdefrder

--- main.js
+++ main.js
@@ -25,22 +25,22 @@
           xhr.send();
         })
       };
-      get().then(function(data){
+      get('https://free.currencyconverterapi.com/api/v5/currencies').then(function(data){
           console.log('Success',data);
-        //   let currencies = data.results;
+          let currencies = data.results;
         
-        //   for(c in currencies){ 
-        //     let option=document.createElement('option');
-        //     option.value = `${currencies[c].id}`;  
-        //     let check = currencies[c].id;
-        //     if(typeof check === 'undefined'){
-        //       check ='';
+          for(c in currencies){ 
+            let option=document.createElement('option');
+            option.value = `${currencies[c].id}`;  
+            let check = currencies[c].id;
+            if(typeof check === 'undefined'){
+              check ='';
             
-        //     }
-        //     option.text =  ` ${check} (${currencies[c].currencyName})`;  
-        //     expect.appendChild(option);
-        //     have.appendChild(option.cloneNode(true));
-        //   }
+            }
+            option.text =  ` ${check} (${currencies[c].currencyName})`;  
+            expect.appendChild(option);
+            have.appendChild(option.cloneNode(true));
+          }
       }).catch(function(err){
           console.log('Err');
       })

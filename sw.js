b3f0456e7eac5c9463e684126d53fc9ca781f522
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 11:50:38 +0100

    ressdefderdedefrhfirifr8gfrg20

--- sw.js
+++ sw.js
@@ -3,10 +3,9 @@ var cacheFiles = [
                   'node_modules/bootstrap/dist/css/bootstrap.css',
                   'node_modules/bootstrap/dist/js/bootstrap.js',
                   'node_modules/jquery/dist/jquery.js',
-                  '/',
-                  'sw.js',
-                  'main.js',
-                  'https://free.currencyconverterapi.com/api/v5/currencies'
+                  'https://free.currencyconverterapi.com/api/v5/currencies',
+                  'index.html',
+                  'main.js'
                   ]
 	self.addEventListener('install',function(e){
 	  // Perform install step

475a9f2787183a43475735fac78c4e5e929646fc
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 10:24:02 +0100

    Checkinssdess

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
 
-    navigator.serviceWorker.register('sw.js').then(function(reg){
-        console.log('ServiceWorker Registered');
-      }).catch(function(err){
-        console.log('Not registered',err);
-      })
- 
     
